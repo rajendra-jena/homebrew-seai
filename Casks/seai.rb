3a800e8e317fa7d0e5c8b268a433fe6f5a8cfbb1
Rajendra Narayan Jena <45740186+rajendra-jena@users.noreply.github.com>
2026-03-04 22:20:57 -0600

    Update seai.rb

--- Casks/seai.rb
+++ Casks/seai.rb
@@ -2,18 +2,18 @@ cask "seai" do
   version "1.0.0"
 
   on_arm do
-    url "https://github.com/rajendra-jena/seai/releases/download/v#{version}/se.AI.By.Infosys-darwin-arm64-#{version}.zip"
+    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/se.AI.By.Infosys-darwin-arm64-#{version}.zip"
     sha256 "9c9b577ac96ae59bb10ce00a861276f6849d119cce835af6428280625b479401"
   end
 
   on_intel do
-    url "https://github.com/rajendra-jena/seai/releases/download/v#{version}/se.AI.By.Infosys-darwin-x64-#{version}.zip"
+    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/se.AI.By.Infosys-darwin-x64-#{version}.zip"
     sha256 "REPLACE_WITH_X64_SHA256"
   end
 
   name "se.AI By Infosys"
   desc "AI agent for software engineering tasks"
-  homepage "https://github.com/rajendra-jena/seai/"
+  homepage "https://github.com/rajendra-jena/seai-release/"
 
   livecheck do
     url :url
