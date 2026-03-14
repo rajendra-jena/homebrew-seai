260923af668bbbf4d99cc86e5399f6f1490ffafa
Rajendra Narayan Jena <45740186+rajendra-jena@users.noreply.github.com>
2026-03-14 14:32:18 -0500

    Update seai.rb

--- Casks/seai.rb
+++ Casks/seai.rb
@@ -2,16 +2,16 @@ cask "seai" do
   version "1.0.0"
 
   on_arm do
-    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/se.AI.By.Infosys-darwin-arm64-#{version}.zip"
+    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/.zip"
     sha256 "8E978FC035DA6DF55C7792A5DC321B94108C97E6C911D8A53166B7DE2277B36A"
   end
 
   on_intel do
-    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/se.AI.By.Infosys-darwin-x64-#{version}.zip"
+    url "https://github.com/rajendra-jena/seai-release/releases/download/v#{version}/.zip"
     sha256 "REPLACE_WITH_X64_SHA256"
   end
 
-  name "se.AI By Infosys"
+  name ""
   desc "AI agent for software engineering tasks"
   homepage "https://github.com/rajendra-jena/seai-release/"
 
@@ -22,10 +22,10 @@ cask "seai" do
 
   app "se.AI By Infosys.app"
 
-  binary "#{appdir}/se.AI By Infosys.app/Contents/Resources/bin/seai"
-  binary "#{appdir}/se.AI By Infosys.app/Contents/Resources/bin/seaid"
+  binary "#{appdir}/apx.app/Contents/Resources/bin/seai"
+  binary "#{appdir}apx.app/Contents/Resources/bin/seaid"
 
-  uninstall quit: "com.infosys.seai"
+  uninstall quit: "com.apx.seai"
 
   zap trash: [
     "~/Library/Application Support/seai",
